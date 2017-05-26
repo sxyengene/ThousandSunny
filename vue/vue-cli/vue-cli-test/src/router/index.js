1a761b43032446148473e76dcaa91de3747bb0e2
sxy <sxyengene@163.com>
2017-05-26 23:13:18 +0800

    test fail

--- vue/vue-cli/vue-cli-test/src/router/index.js
+++ vue/vue-cli/vue-cli-test/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Test from '@/components/test'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/test',
+      component: Test
     }
   ]
 })
